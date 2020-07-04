b4c27c8270f7433587577aa89745c091a8cbcae0
Administrador <Administrador@DESKTOP-3S7B382>
2020-07-04 11:35:51 -0300

    Alterações nos padrões de cores e criação da página de maiores informações

--- Componentes/CMD_CORAC.js
+++ Componentes/CMD_CORAC.js
@@ -109,10 +109,54 @@ class Commands extends JSController{
     }
     Show_BarraLateral(){
         let Self = this;
-        $(".page-wrapper").append("<div id='Barra_InfoContainer'><div id='Regulador_Info'><i class='fas fa-angle-double-left RegulaWidth'></i></div><div id='Conteudo_info'></div></div>");
+        $(".page-wrapper").append("\
+                                    <div id='Barra_InfoContainer'>\n\
+                                        <div id='Regulador_Info'>\n\
+                                            <i class='fas fa-angle-double-left RegulaWidth'></i>\n\
+                                        </div>\n\
+                                        <div id='Conteudo_info'>\n\
+                                            <div id='C_info_Cabecalho'>\n\
+                                                <div id='C_inf_Img'></div>\n\
+                                                <div id='C_inf_Desc'></div>\n\
+                                            </div>\n\
+                                            <div id='C_inf_Conteiner_Geral'>\n\
+                                                <div id='C_inf_C_Geral_botoes_Container'><div id='BarraContainer' class='col-12 d-flex no-block align-items-center' data-original-title='' title=''><nav id='Barra_Icones_GetInfo' class='menu-navigation-claro'></nav></div></div>\n\
+                                                <div id='C_inf_C_Geral_Desc_Container'></div>\n\
+                                            </div>\n\
+                                        </div>\n\
+                                    </div>");
         $("#Regulador_Info").click(function(event){
             Self.MinMax_Barra_Lateral();
         })
+        this.addBotoes();
+    }
+    
+    addBotoes(){
+        
+        let Bt = [
+                    {"IDX":0, "Nome":"Rede", "Funcao":"", "Icone": this.Icones("Rede") ,"Etiqueta":"Rede","Descricao":"Busca informações sobre as configurações de hardware e software da placa de rede da máquina.", "Active":false},
+                    {"IDX":1, "Nome":"DiscoRigido", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":2, "Nome":"Memória", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":3, "Nome":"Processador", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":4, "Nome":"Vídeo", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":5, "Nome":"Teclado", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":6, "Nome":"Mouse", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+                    {"IDX":7, "Nome":"Áudio", "Funcao":"", "Icone": this.Icones("HD") ,"Etiqueta":"Hard Disc","Descricao":"Busca informações sobre os discos da máquina.", "Active":false},
+               ]
+
+        let Modelo_Botao = "<a href='#' class='{Class}' id='{Nome}' data-index={IDX} alt='{Descricao}'>{Icone}<span>{Etiqueta}</span></a>"
+        
+        for(var i in Bt){
+            let _IDX = Modelo_Botao.replace("{IDX}",Bt[i].IDX);
+            let _Nome = _IDX.replace("{Nome}",Bt[i].Nome);
+            let _Funcao = _Nome.replace("{Funcao}",Bt[i].Funcao);
+            let _Icone = _Funcao.replace("{Icone}",Bt[i].Icone);
+            let _Etiqueta = _Icone.replace("{Etiqueta}",Bt[i].Etiqueta);
+            let _Descricao = _Etiqueta.replace("{Descricao}",Bt[i].Descricao);
+            let _BotaoCompleto = _Descricao.replace("{Active}",Bt[i].Active);
+            
+        $("#Barra_Icones_GetInfo").append(_BotaoCompleto)   
+        }
     }
     
     /**
@@ -134,6 +178,13 @@ class Commands extends JSController{
                 bootbox.alert("<h3>"+ Erros.Mensagem +"</h3>");
                 break;
         }
+    }
+    
+    Icones(IDX){
+        var I = []
+        I["Rede"] = '<svg class="icon_inf" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="network-wired" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-network-wired fa-w-20 fa-7x"><path fill="currentColor" d="M632 240H336v-80h80c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32H224c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h80v80H8c-4.42 0-8 3.58-8 8v16c0 4.42 3.58 8 8 8h120v80H64c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h160c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32h-64v-80h320v80h-64c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h160c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32h-64v-80h120c4.42 0 8-3.58 8-8v-16c0-4.42-3.58-8-8-8zM224 384v96H64v-96h160zm0-256V32h192v96H224zm352 256v96H416v-96h160z" class=""></path></svg>';
+        I["HD"] = '<svg  class="icon_inf" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="disc-drive" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-disc-drive fa-w-16 fa-7x"><path fill="currentColor" d="M256 224a32 32 0 1 0 32 32 32 32 0 0 0-32-32zm248 224h-24V96a64 64 0 0 0-64-64H96a64 64 0 0 0-64 64v352H8a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h496a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8zm-56 0H64V96a32 32 0 0 1 32-32h320a32 32 0 0 1 32 32zM256 96a160 160 0 1 0 160 160A160 160 0 0 0 256 96zm0 288a128 128 0 1 1 128-128 128.14 128.14 0 0 1-128 128z" class=""></path></svg>'
+        return I[IDX]
     }
 }
 
